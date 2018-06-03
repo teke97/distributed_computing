6ef8ba398fe1a7fed5f5a3ec09d248a547a95617
Yaros <teke97@gmail.com>
2018-06-03 20:30:14 +0300

    minimal relation

--- src/lab1.c
+++ src/lab1.c
@@ -7,17 +7,29 @@
 #include "lib/pa1_starter_code/pa1.h"
 #include <sys/types.h>
 #include <sys/wait.h>
-
+#include <malloc.h>
 
 void usage(){
 	printf("Usage: lab1 -P X,\n\twhere X is a number of process (0<X<=10)\n");
 }
 
+int send(void* self, local_id dst, const Message * msg){
+	printf("%i", *((int*)self + dst));
+}
 
+int child_work(int* fd, int id, int process_number){
+	printf(log_started_fmt, id, getpid(), getppid());
+	send(fd, 2, "heh");
+	printf(log_done_fmt, id);
+}
+
+
+int parent_work(){}
 
 int main(int argc, char *argv[]) {
 	int process_number;
 	int* pid;
+	int* fd;
 	if (argc != 3){
 		usage();
 		return 1;
@@ -28,21 +40,27 @@ int main(int argc, char *argv[]) {
 	}
 
 	process_number = atoi(argv[2]);
-	pid = (int*) calloc(process_number, sizeof(int)); 
 
 	if (process_number<=0 || process_number > 10){
 		usage();
 		return 3;
 	}
 
+	fd = (int*) malloc ((process_number + 1) ^ 2 * sizeof(int));
+	for (int j = 0; j < process_number; j++)
+		for (int i = 0; i < process_number; i++){
+			if(pipe(fd + i * 2 + j * process_number) != 0)
+				return 4;
+		}
+
+	pid = (int*) calloc(process_number, sizeof(int)); 
+
 	for (int i = 0; i < process_number; i++){
 		pid[i] = fork();
 		if( pid[i] == 0) {
-			printf(log_started_fmt, i, getpid(), getppid());
-			printf(log_done_fmt, i);
-			return 0;	
+			return child_work(fd, i, process_number);
 		}
 	}
-	
+	parent_work();
 	while ( wait(NULL) > 0);
 }
